5855f2dc3d85f42939db1b7b583f1dc03a1fc479
Travis <tp523130@gmail.com>
2019-01-13 20:44:43 -0800

    Heroku not allowing 2 ports.. :(

--- index.js
+++ index.js
@@ -30,14 +30,14 @@ var server = app.listen(process.env.PORT || 5000, function () {
     console.log("Portfolio is working on port " + port);
 });
 
-var app2 = express();
-const PORT2 = process.env.PORT || 4000;
+// var app2 = express();
+// const PORT2 = process.env.PORT || 4000;
 
-app2.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+"/App2/views/index.html"))
-});
+// app2.get('/', function(req, res){
+//     res.sendFile(path.join(__dirname+"/App2/views/index.html"))
+// });
 
-var server2 = app2.listen(process.env.PORT || 4000, function () {
-    var port2 = server2.address().port;
-    console.log("Portfolio is working on port " + port2);
-});
+// var server2 = app2.listen(process.env.PORT || 4000, function () {
+//     var port2 = server2.address().port;
+//     console.log("Portfolio is working on port " + port2);
+// });
