76bfaef0027b427d72e31ef1dac48bdc3e09afbb
Travis <tp523130@gmail.com>
2019-01-13 19:53:03 -0800

    working on dual servers

--- index.js
+++ index.js
@@ -29,3 +29,15 @@ var server = app.listen(process.env.PORT || 5000, function () {
     var port = server.address().port;
     console.log("Portfolio is working on port " + port);
 });
+
+var app2 = express();
+const PORT2 = process.env.PORT || 4000;
+
+app2.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+"/App2/views/index.html"))
+});
+
+var server2 = app2.listen(process.env.PORT || 4000, function () {
+    var port2 = server2.address().port;
+    console.log("Portfolio is working on port " + port2);
+});
