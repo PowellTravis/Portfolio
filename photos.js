49168f5a71a99fcbe1fb933178e38f1847c05c1f
Travis <tp523130@gmail.com>
2019-01-22 20:37:13 -0800

    Remotes are great 😒

--- photos.js
+++ photos.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+"/Photos/views/index.html"))
 });
 
+app.get('/instagram', function(req, res){
+    res.sendFile(path.join(__dirname+"/Photos/views/instagram.html"))
+});
+
 var server = app.listen(process.env.PORT || 3000, function () {
     var port = server.address().port;
     console.log("Photos is working on port " + port);
