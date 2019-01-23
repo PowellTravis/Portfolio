485b84acfec20cc002070b12941cb0374d3a9d82
Travis <tp523130@gmail.com>
2019-01-22 21:04:36 -0800

    It looks pretty good, lets see if this works

--- photos.js
+++ photos.js
@@ -12,12 +12,6 @@ app.get('/instagram', function(req, res){
     res.sendFile(path.join(__dirname+"/Photos/views/instagram.html"))
 });
 
-
-
-
-
-
-
 var server = app.listen(process.env.PORT || 3000, function () {
     var port = server.address().port;
     console.log("Photos is working on port " + port);
